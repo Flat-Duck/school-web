161661dd1b6bdfa3d8819ac656750bb287223b31
Abdulrahman Mahidwei <a.mahidwei@outlook.com>
2023-01-05 02:22:13 +0200

    fix

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -87,7 +87,7 @@ return [
     'min' => [
         'numeric' => 'The :attribute must be at least :min.',
         'file' => 'The :attribute must be at least :min kilobytes.',
-        'string' => 'The :attribute must be at least :min characters.',
+        'string' => 'حقل :attribute يجب ان لايقل عن :min خانات.',
         'array' => 'The :attribute must have at least :min items.',
     ],
     'not_in' => 'The selected :attribute is invalid.',
